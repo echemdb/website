a1c79f12f74d63b2170ecd348b14ffeccd25868a
Julian Rüth <julian.rueth@fsfe.org>
2021-10-19 11:56:08 +0200

    add license headers

--- echemdb/data/cv/database.py
+++ echemdb/data/cv/database.py
@@ -1,3 +1,24 @@
+# ********************************************************************
+#  This file is part of echemdb.
+#
+#        Copyright (C) 2021 Albert Engstfeld
+#        Copyright (C) 2021 Johannes Hermann
+#        Copyright (C) 2021 Julian Rüth
+#        Copyright (C) 2021 Nicolas Hörmann
+#
+#  echemdb is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  echemdb is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with echemdb. If not, see <https://www.gnu.org/licenses/>.
+# ********************************************************************
 class Database:
     def __init__(self, data_packages):
         self._packages = data_packages
