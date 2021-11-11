2699340549814faa237cc2dc97324769f22053ea
Albert Engstfeld <albert.engstfeld@gmail.com>
2021-11-12 00:36:44 +0100

    Adapted doctest

--- echemdb/data/cv/entry.py
+++ echemdb/data/cv/entry.py
@@ -228,13 +228,11 @@ class Entry:
 
             >>> entry = Entry.create_examples()[0]
             >>> entry.electrochemical_system_metadata()
-            [Entry('Engstfeld_2018_polycrystalline_17743_4b_1')]
+            'electrodes:\n  configuration: three\n  counter electrode:\n    crystallographic orientation: poly\n    material: Au\n    shape: mesh\n  reference electrode:\n    source:\n      supplier: homemade\n    type: RHE\n  working electrode:\n    crystallographic orientation: 100\n    geometric electrolyte contact area:\n      unit: cm-2\n      value: null\n    material: Cu\n    preparation procedure: sputter and heating under UHV conditions\n    shape:\n      diameter:\n        unit: mm\n        value: 4.4\n      height:\n        unit: mm\n        value: 2\n      type: head shaped\n    source:\n      LOT: null\n      supplier: Mateck\nelectrolyte:\n  components:\n  - name: water\n    proportion:\n      unit: volume percent\n      value: 100\n    source:\n      quality: ultrapure water\n      refinement: Millipore MilliQ\n      total organic carbon:\n        unit: none\n        value: none\n    sum formula: H2O\n    type: solvent\n  - concentration:\n      unit: M\n      value: 0.1\n    name: potassium hydroxide\n    source:\n      LOT: null\n      supplier: Merck Suprapur\n    sum formula: KOH\n    type: alkaline\n  ph:\n    uncertainty: none\n    value: 13\n  temperature:\n    unit: K\n    value: 298.15\n  type: aq\n'
 
         '''
         import yaml
-        meta = self.package.to_dict()
-        import markdown_strings
-        #return markdown_strings.code_block(yaml.dump(meta['electrochemical system']))
+        meta = self.package.to_dict() # `to_dict` is apparently deprecated. Find another way to turn the package into a dict.
         return yaml.dump(meta['electrochemical system'])
 
 class Descriptor:
