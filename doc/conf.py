808574f7b18ab0b1e4ad949e58c32a97b9d0f9a5
Albert Engstfeld <albert.engstfeld@gmail.com>
2022-02-04 22:06:38 +0100

    Setting up Sphinx docu

---
+++ doc/conf.py
@@ -0,0 +1,32 @@
+project = 'echemdb'
+copyright = '2021, the echemdb website authors'
+author = 'the echemdb authors'
+
+release = '0.0.0'
+
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo", "myst_nb"]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.ipynb': 'myst-nb',
+    '.myst': 'myst-nb',
+}
+
+templates_path = ['_templates']
+
+exclude_patterns = ['generated', 'Thumbs.db', '.DS_Store', 'README.md', 'news', '.ipynb_checkpoints', '*.ipynb']
+
+todo_include_todos = True
+
+html_theme = 'sphinx_rtd_theme'
+
+html_static_path = []
+
+# Add Edit on GitHub links
+html_context = {
+    'display_github': True,
+    'github_user': 'echemdb',
+    'github_repo': 'website',
+    'github_version': 'master/doc/',
+}
---
+++ doc/index.md
@@ -0,0 +1,14 @@
+Welcome to echemdb's documentation!
+========================================
+
+```{todo}
+* what is echemdb and what is our aim.
+* then refer to installation, cli, api and cv.
+```
+
+```{toctree}
+:maxdepth: 2
+:caption: "Contents:"
+:hidden:
+api.md
+```
