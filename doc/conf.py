dea2d8e36d1f22f26f60980a1236c24399442ff3
Albert Engstfeld <albert.engstfeld@gmail.com>
2022-02-07 13:55:56 +0100

    disable linkchecker

--- doc/conf.py
+++ doc/conf.py
@@ -24,9 +24,9 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = []
 
 # Add Edit on GitHub links
-html_context = {
-    'display_github': True,
-    'github_user': 'echemdb',
-    'github_repo': 'website',
-    'github_version': 'main/doc/',
-}
+# html_context = {
+#     'display_github': True,
+#     'github_user': 'echemdb',
+#     'github_repo': 'website',
+#     'github_version': 'main/doc/',
+# }
