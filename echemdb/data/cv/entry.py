e0877b6039143f8bcb8194a23ed9457a841ee054
Albert Engstfeld <albert.engstfeld@gmail.com>
2022-03-04 22:28:56 +0100

    Fix doctest dir

--- echemdb/data/cv/entry.py
+++ echemdb/data/cv/entry.py
@@ -95,8 +95,17 @@ class Entry:
         EXAMPLES::
 
             >>> entry = Entry.create_examples()[0]
-            >>> dir(entry)
-            ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattr__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_descriptor', 'bibliography', 'citation', 'create_examples', 'curation', 'data_description', 'df', 'experimental', 'figure_description', 'identifier', 'package', 'plot', 'profile', 'resources', 'source', 'system', 'version', 'x', 'x_unit', 'y', 'y_unit', 'yaml']
+            >>> dir(entry) # doctest: +NORMALIZE_WHITESPACE
+            ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__',
+            '__eq__', '__format__', '__ge__', '__getattr__', '__getattribute__',
+            '__getitem__', '__gt__', '__hash__', '__init__', '__init_subclass__',
+            '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__',
+            '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__',
+            '__subclasshook__', '__weakref__', '_descriptor', '_verify_field_name',
+            'bibliography', 'citation', 'create_examples', 'curation', 'data_description',
+            'df', 'experimental', 'field_names', 'field_unit', 'figure_description',
+            'identifier', 'package', 'plot', 'profile', 'rescale', 'rescale_original',
+            'resources', 'source', 'system', 'version', 'yaml']
 
         """
         return list(set(dir(self._descriptor) + object.__dir__(self)))
@@ -108,8 +117,9 @@ class Entry:
         EXAMPLES::
 
             >>> entry = Entry.create_examples()[0]
-            >>> entry.source
-            {'citation key': 'alves_2011_electrochemistry_6010', 'curve': 'solid', 'url': 'https://doi.org/10.1039/C0CP01001D', 'figure': '2a', 'version': 1}
+            >>> entry.source # doctest: +NORMALIZE_WHITESPACE
+            {'citation key': 'alves_2011_electrochemistry_6010', 'curve': 'solid',
+            'url': 'https://doi.org/10.1039/C0CP01001D', 'figure': '2a', 'version': 1}
 
         The returned descriptor can again be accessed in the same way::
 
@@ -126,8 +136,9 @@ class Entry:
         EXAMPLES::
 
             >>> entry = Entry.create_examples()[0]
-            >>> entry["source"]
-            {'citation key': 'alves_2011_electrochemistry_6010', 'curve': 'solid', 'url': 'https://doi.org/10.1039/C0CP01001D', 'figure': '2a', 'version': 1}
+            >>> entry["source"] # doctest: +NORMALIZE_WHITESPACE
+            {'citation key': 'alves_2011_electrochemistry_6010', 'curve': 'solid',
+            'url': 'https://doi.org/10.1039/C0CP01001D', 'figure': '2a', 'version': 1}
 
         """
         return self._descriptor[name]
