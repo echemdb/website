8f103b990f90b934acd650cf791ed5508dab0656
Albert Engstfeld <albert.engstfeld@gmail.com>
2022-03-15 12:38:47 +0100

    Include utility module to create datapackages

---
+++ echemdb/data/cv/create.py
@@ -0,0 +1,26 @@
+r"""
+Utilities to create datapackages for echemdb from datapackages (CSV and JSON), 
+with svgdigitizer (SVG and YAML) or from raw files (CSV and YAML).
+
+Also includes utilities to collect bibliography entries and store them
+along with the generated datapackage.
+"""
+
+# ********************************************************************
+#  This file is part of echemdb.
+#
+#        Copyright (C) 2022 Albert Engstfeld
+#
+#  echemdb is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  echemdb is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with echemdb. If not, see <https://www.gnu.org/licenses/>.
+# ********************************************************************
