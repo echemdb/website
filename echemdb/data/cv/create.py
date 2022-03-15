2a4e10ac36fbb22cea6dd279458c8ad9e71c3b83
Albert Engstfeld <albert.engstfeld@gmail.com>
2022-03-15 13:27:13 +0100

    Added create from SVG function based on the code in entry.create_examples

--- echemdb/data/cv/create.py
+++ echemdb/data/cv/create.py
@@ -24,3 +24,62 @@ along with the generated datapackage.
 #  You should have received a copy of the GNU General Public License
 #  along with echemdb. If not, see <https://www.gnu.org/licenses/>.
 # ********************************************************************
+
+def create_from_SVG(sourcedir=None, outdir=None, name=None, replace_files=False, sampling=.005):
+    """
+    Creates a datapackage, consisting of a CSV and JSON, 
+    from an SVG (prepared with ``svgdigitizer``) and a YAML.
+    Datapackages can be created from individual files or 
+    all files in a specified folder.
+
+    The default sourcedir is `./literature/svg/` in the basepath of echemdb.
+    The default outdir is `./data/generated/svgdigitizer/` in the basepath of echemdb.
+
+    `name` is the name of the folder of a specific file.
+    
+    """
+    import os.path
+
+    sourcedir = sourcedir or os.path.join(
+        os.path.dirname(__file__),
+        '..',
+        '..',
+        '..',
+        'literature',
+        name or '')
+
+    if not os.path.exists(sourcedir):
+        raise ValueError(f"Could not find sourcedir {sourcedir}.")
+
+    outdir = os.path.join(
+        os.path.dirname(__file__),
+        '..',
+        '..',
+        '..',
+        'data',
+        'generated',
+        'svgdigitizer',
+        name or '')
+
+    # We now might have to digitize some files on demand. When running
+    # tests in parallel, this introduces a race condition that we avoid
+    # with a global lock in the file system.
+    lockfile = f"{outdir}.lock"
+    os.makedirs(os.path.dirname(lockfile), exist_ok=True)
+
+    from filelock import FileLock
+    with FileLock(lockfile):
+        if not replace_files:
+            if os.path.exists(outdir):
+                raise Exception(f"The directory {outdir} exist. Delete the directory or force overwriting files by setting `replace_files=True`")
+
+        from glob import glob
+        for yaml in glob(os.path.join(sourcedir, "*.yaml")):
+            svg = os.path.splitext(yaml)[0] + ".svg"
+
+            from svgdigitizer.test.cli import invoke
+            from svgdigitizer.__main__ import digitize_cv
+            invoke(digitize_cv, "--sampling-interval", str(sampling), "--package", "--metadata", yaml, svg, "--outdir", outdir)
+
+        assert os.path.exists(outdir), f"Ran digitizer to generate {outdir}. But directory is still missing after invoking digitizer."
+        assert any(os.scandir(outdir)), f"Ran digitizer to generate {outdir}. But the directory generated is empty after invoking digitizer."
