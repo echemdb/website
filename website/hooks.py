ecc2ee09ecb99bb018e3478dc455484ead5accde
linuxrider <linuxrider@web.de>
2022-06-26 07:53:18 +0200

    fix lints

--- website/hooks.py
+++ website/hooks.py
@@ -1,7 +1,8 @@
+"""Hooks for local usage."""
 import os
 import shutil
 
-def copy_data(config, **kwargs):
+def copy_data(config):
+    """Copy the data to the website folder."""
     site_dir = config['site_dir']
-    #shutil.copy('data/generated/svgdigitizer/alves_2011_electrochemistry_6010/alves_2011_electrochemistry_6010_f1a_solid.csv', os.path.join(site_dir, 'alves_2011_electrochemistry_6010_f1a_solid.csv'))
     shutil.copytree('data', os.path.join(site_dir, 'data'))
