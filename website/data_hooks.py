125ab9773998390df47c21b44ad9c5f6416921d8
linuxrider <linuxrider@web.de>
2025-08-18 17:37:03 +0200

    remove deprecated simple-hooks for mkdocs

--- website/data_hooks.py
+++ website/data_hooks.py
@@ -1,10 +1,16 @@
 """Hooks for local usage."""
 
+import logging
 import os
 import shutil
 
+import mkdocs.plugins
 
-def copy_data(config):
+log = logging.getLogger("mkdocs")
+
+
+@mkdocs.plugins.event_priority(-50)
+def on_post_build(config):
     """Copy the data to the website folder."""
     site_dir = config["site_dir"]
     shutil.copytree("data", os.path.join(site_dir, "data"))
