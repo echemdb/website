398174da720cc33013d5402ae294e52d33eb41fc
Johannes Hermann <linuxrider@web.de>
2025-08-29 22:54:47 +0200

    black format

--- website/filters/render.py
+++ website/filters/render.py
@@ -148,4 +148,8 @@ def render_plot(entry):
     fig.update_yaxes(
         showline=True, mirror=True, range=(y_min - y_padding, y_max + y_padding)
     )
-    return fig.to_html(full_html=False, include_plotlyjs="https://cdn.plot.ly/plotly-3.1.0.min.js", include_mathjax=False)
+    return fig.to_html(
+        full_html=False,
+        include_plotlyjs="https://cdn.plot.ly/plotly-3.1.0.min.js",
+        include_mathjax=False,
+    )
