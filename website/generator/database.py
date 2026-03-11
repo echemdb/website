e0a7c3f84dc7dbf76de1c4cd92395633d4c2f18d
Albert Engstfeld <albert.engstfeld@gmail.com>
2026-03-11 14:24:45 +0100

    upgrade to version 0.7.7

--- website/generator/database.py
+++ website/generator/database.py
@@ -41,7 +41,7 @@ from unitpackage.database.echemdb import Echemdb
 
 ECHEMDB_DATABASE_URL = os.environ.get(
     "ECHEMDB_DATABASE_URL",
-    "https://github.com/echemdb/electrochemistry-data/releases/download/0.7.6/data-0.7.6.zip",
+    "https://github.com/echemdb/electrochemistry-data/releases/download/0.7.7/data-0.7.7.zip",
 )
 
 cv = Echemdb.from_remote(url=ECHEMDB_DATABASE_URL)
